557923a34b7289bc84013d47e4495f2a13fa5f23
Chris Williams <luciousvault12@gmail.com>
2018-01-10 19:56:58 -0500

    Updating analyzer with popularity query

--- news_analyzer.py
+++ news_analyzer.py
@@ -1,25 +1,38 @@
 import psycopg2
 
-CONN_STRING = """host='localhost' dbname='news' user='vagrant' password='vagrant'"""
-
 LIST_TABLES = """SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
     WHERE TABLE_CATALOG = 'news' AND TABLE_SCHEMA = 'public';"""
+POPULARITY_QUERY = """SELECT title, COUNT(log.id) as popularity
+                        FROM log, articles
+                        WHERE CONCAT('/article/',articles.slug) = log.path
+                        GROUP BY title
+                        ORDER BY popularity DESC
+                        LIMIT 3;"""
 
 # method intended to run "safe" select queries
-def sql_select(query):
+def connect():
     try:
-        dbconn = psycopg2.connect(dbname='news')
-    except:
-        print("Failed to connect to the news db")
-        return
-    cursor = dbconn.cursor()
-    cursor.execute(query)
-    return cursor.fetchall()
-    dbconn.close()
+        dbconn = psycopg2.connect(dbname="news")
+        cursor = dbconn.cursor()
+        return dbconn, cursor
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def select_query(query):
+    my_dbconn, my_cursor = connect()
+    my_cursor.execute(LIST_TABLES)
+    my_cursor.execute(POPULARITY_QUERY)
+    return my_cursor.fetchall()
+    my_dbconn.close()
 
 def test_print_tables():
     tables = sql_select(LIST_TABLES)
     print(tables)
 
+def print_popular_articles():
+    popularity_data = sql_select(POPULARITY_QUERY)
+    print(popularity_data)
+
 if __name__ == '__main__':
     test_print_tables()
+#    print_popular_articles()
