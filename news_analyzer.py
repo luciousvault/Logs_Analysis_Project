d3f61e295acfacd5da323e60649627e9a6c77c47
Chris Williams <luciousvault12@gmail.com>
2018-01-12 20:16:36 -0500

    Updated analyzer with string formatting2

--- news_analyzer.py
+++ news_analyzer.py
@@ -36,33 +36,38 @@ def connect():  # method intended to run "safe" select queries
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
 
+
 def select_query(query):
     my_dbconn, my_cursor = connect()
     my_cursor.execute(query)
     return my_cursor.fetchall()
     my_dbconn.close()
 
+
 def test_print_tables():
     tables = select_query(LIST_TABLES)
     print(tables)
 
+
 def print_popular_articles():
     popularity_data = select_query(POPULARITY_QUERY)
     article_row_format = '"{}" — {} views'
-    for row in popularity_data
+    for row in popularity_data:
         print(article_row_format.format(row[0], row[1]))
+
 
 def print_popular_authors():
     views_data = select_query(AUTHORS_VIEWS_QUERY)
     author_row_format = '{} - {} views'
-    for row in views_data
-        print(author_row_format.format(row[0],row[1]))
-    print(views_data)
+    for row in views_data:
+        print(author_row_format.format(row[0], row[1]))
+
 
 def print_highest_error_percent():
     error_data = select_query(ERROR_PERCENT_QUERY)
     row = error_data[0]
-    print('{} - {}%% errors'.format(row[1].strftime('%b %d, %Y'),row[0]))
+    print('{} - {}%% errors'.format(row[1].strftime('%b %d, %Y'), row[0]))
+
 
 if __name__ == '__main__':
     test_print_tables()
